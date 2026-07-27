0dc6bbf5d655ece80acfc99f239a2a6c439f6edd
SHIVANSH SAHU <shivanshsahu0808@gmail.com>
2026-07-27 18:40:40 +0530

    Update Controller.java

--- src/main/java/com/shivansh/weather_app/controller/Controller.java
+++ src/main/java/com/shivansh/weather_app/controller/Controller.java
@@ -6,7 +6,7 @@ import com.shivansh.weather_app.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/weather")
 public class Controller {
